8f497cbafc857b537f52cba4c4a1574be4467883
davisonb <davisonb@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2011-03-16 12:29:53 +0000

    Bruce made some changes to Muluneh's branch to change the waterbalance.csv file
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/Branches/MESH_CODE_TEAM/Muluneh/TRUNK@441 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/MESH_driver.f90
+++ Driver/MESH_Driver/MESH_driver.f90
@@ -572,7 +572,8 @@ REAL :: TOTAL_ROFACC, TOTAL_ROFOACC, TOTAL_ROFSACC, &
   TOTAL_ROFBACC, TOTAL_EVAPACC, TOTAL_PREACC, INIT_STORE, &
   FINAL_STORE, TOTAL_AREA
 
-REAL :: TOTAL_STORE,TOTAL_THLQ(6),TOTAL_THIC(6),TOTAL_ZPND,TOTAL_RCAN,TOTAL_SCAN
+REAL :: TOTAL_STORE,TOTAL_THLQ(6),TOTAL_THIC(6),TOTAL_ZPND,TOTAL_RCAN,TOTAL_SCAN,TOTAL_SNO
+REAL :: TOTAL_PRE,TOTAL_EVAP,TOTAL_ROF,TOTAL_ROFO,TOTAL_ROFS,TOTAL_ROFB
 
 !> CROSS-CLASS VARIABLES (CLASS):
 !> ARRAYS DEFINED TO PASS INFORMATION BETWEEN THE THREE MAJOR
@@ -1682,6 +1683,12 @@ DO I=1,NA
 ENDDO !DO I=1,NA
 
 !> clear accumulating variables
+TOTAL_ROF=0.0
+TOTAL_ROFO=0.0
+TOTAL_ROFS=0.0
+TOTAL_ROFB=0.0
+TOTAL_EVAP=0.0
+TOTAL_PRE=0.0
 TOTAL_ROFACC=0.0
 TOTAL_ROFOACC=0.0
 TOTAL_ROFSACC=0.0
@@ -2632,10 +2639,12 @@ TOTAL_THIC  = 0.0
 TOTAL_ZPND  = 0.0
 TOTAL_RCAN  = 0.0
 TOTAL_SCAN  = 0.0
-OPEN(900,FILE = 'Basin_average_water_balance.csv')
-WRITE(900,"('IDAY,IYEAR,PREACC,EVAPACC,ROFACC,ROFOACC,ROFSACC,ROFBACC,SCAN,RCAN,ZPND,"// &
+TOTAL_SNO   = 0.0
+OPEN(unit=900,file="./" // GENDIR_OUT(1:INDEX(GENDIR_OUT," ")-1) // &
+                  '/Basin_average_water_balance.csv')
+WRITE(900,"('IDAY,IYEAR,PREACC,EVAPACC,ROFACC,ROFOACC,ROFSACC,ROFBACC,PRE,EVAP,ROF,ROFO,ROFS,ROFB,SNO,SCAN,RCAN,ZPND,"// &
             "THLQ1,THLQ2,THLQ3,THLQ4,THLQ5,THLQ6,THIC1,THIC2,THIC3,THIC4,THIC5,THIC6,"// &
-            "THLQIC1,THLQIC2,THLQIC3,THLQIC4,THLQIC5,THLQIC6,THLQ,THLIC,THLQIC,STORAGE')")
+            "THLQIC1,THLQIC2,THLQIC3,THLQIC4,THLQIC5,THLQIC6,THLQ,THLIC,THLQIC,STORAGE,DELTA_STORAGE')")
                                                 
 !> *********************************************************************
 !> Start of main loop that is run each half hour
@@ -3605,14 +3614,21 @@ IF(NCOUNT==48) THEN !48 is the last half-hour period of the day
     ENDDO  !DO K=1, WF_NUM_POINTS
 
 !> update components for final tally
-    TOTAL_ROFACC=TOTAL_ROFACC+ROFACC(I)
-    TOTAL_ROFOACC=TOTAL_ROFOACC+ROFOACC(I)
-    TOTAL_ROFSACC=TOTAL_ROFSACC+ROFSACC(I)
-    TOTAL_ROFBACC=TOTAL_ROFBACC+ROFBACC(I)
-    TOTAL_EVAPACC=TOTAL_EVAPACC+EVAPACC(I)
-    TOTAL_PREACC=TOTAL_PREACC+PREACC(I)
-    TOTAL_SCAN  = TOTAL_SCAN + SCANACC(I)
-    TOTAL_RCAN  = TOTAL_RCAN + RCANACC(I)
+    TOTAL_ROF     = TOTAL_ROF     + ROFACC(I)
+    TOTAL_ROFO    = TOTAL_ROFO    + ROFOACC(I)
+    TOTAL_ROFS    = TOTAL_ROFS    + ROFSACC(I)
+    TOTAL_ROFB    = TOTAL_ROFB    + ROFBACC(I)
+    TOTAL_EVAP    = TOTAL_EVAP    + EVAPACC(I)
+    TOTAL_PRE     = TOTAL_PRE     + PREACC(I)
+    TOTAL_ROFACC  = TOTAL_ROFACC  + ROFACC(I)
+    TOTAL_ROFOACC = TOTAL_ROFOACC + ROFOACC(I)
+    TOTAL_ROFSACC = TOTAL_ROFSACC + ROFSACC(I)
+    TOTAL_ROFBACC = TOTAL_ROFBACC + ROFBACC(I)
+    TOTAL_EVAPACC = TOTAL_EVAPACC + EVAPACC(I)
+    TOTAL_PREACC  = TOTAL_PREACC  + PREACC(I)
+    TOTAL_SNO     = TOTAL_SNO     + SNOACC(I)
+    TOTAL_SCAN    = TOTAL_SCAN    + SCANACC(I)
+    TOTAL_RCAN    = TOTAL_RCAN    + RCANACC(I)
 
     IF (WF_NUM_POINTS > 0) THEN !SUMMARY VALUES FOR SCREEN
       DO J = 1, WF_NUM_POINTS !FOR MORE THAN 1 OUTPUT
@@ -3678,22 +3694,30 @@ IF(NCOUNT==48) THEN !48 is the last half-hour period of the day
   TOTAL_STORE = TOTAL_STORE + TOTAL_ZPND + SUM(TOTAL_THLQ(1:IGND)) + SUM(TOTAL_THIC(1:IGND))
   
   WRITE(900,'((I4,","),(I5,","),100(E12.5,","))')IDAY,IYEAR,    &
-                                                TOTAL_PREACC,  &
-                                                TOTAL_EVAPACC, &
-                                                TOTAL_ROFACC,  &
-                                                TOTAL_ROFOACC, &
-                                                TOTAL_ROFSACC, &
-                                                TOTAL_ROFBACC, &
-                                                TOTAL_SCAN,    &
-                                                TOTAL_RCAN,    &
-                                                TOTAL_ZPND,    &
-                                                (TOTAL_THLQ(J), J = 1, IGND), &
-                                                (TOTAL_THIC(J), J = 1, IGND), &
-                                                (TOTAL_THLQ(J) + TOTAL_THIC(J), J = 1, IGND), &
-                                                SUM(TOTAL_THLQ(1:IGND)), &
-                                                SUM(TOTAL_THIC(1:IGND)), &
-                                                SUM(TOTAL_THLQ(1:IGND)) + SUM(TOTAL_THIC(1:IGND)), &
-                                                TOTAL_STORE
+                                                TOTAL_PREACC/TOTAL_AREA,  &
+                                                TOTAL_EVAPACC/TOTAL_AREA, &
+                                                TOTAL_ROFACC/TOTAL_AREA,  &
+                                                TOTAL_ROFOACC/TOTAL_AREA, &
+                                                TOTAL_ROFSACC/TOTAL_AREA, &
+                                                TOTAL_ROFBACC/TOTAL_AREA, &
+                                                TOTAL_PRE/TOTAL_AREA,  &
+                                                TOTAL_EVAP/TOTAL_AREA, &
+                                                TOTAL_ROF/TOTAL_AREA,  &
+                                                TOTAL_ROFO/TOTAL_AREA, &
+                                                TOTAL_ROFS/TOTAL_AREA, &
+                                                TOTAL_ROFB/TOTAL_AREA, &
+                                                TOTAL_SNO/TOTAL_AREA,    &
+                                                TOTAL_SCAN/TOTAL_AREA,    &
+                                                TOTAL_RCAN/TOTAL_AREA,    &
+                                                TOTAL_ZPND/TOTAL_AREA,    &
+                                                (TOTAL_THLQ(J)/TOTAL_AREA, J = 1, IGND), &
+                                                (TOTAL_THIC(J)/TOTAL_AREA, J = 1, IGND), &
+                                                ((TOTAL_THLQ(J) + TOTAL_THIC(J))/TOTAL_AREA, J = 1, IGND), &
+                                                SUM(TOTAL_THLQ(1:IGND))/TOTAL_AREA, &
+                                                SUM(TOTAL_THIC(1:IGND))/TOTAL_AREA, &
+                                                SUM(TOTAL_THLQ(1:IGND)) + SUM(TOTAL_THIC(1:IGND))/TOTAL_AREA, &
+                                                TOTAL_STORE/TOTAL_AREA, &
+                                                (TOTAL_STORE - INIT_STORE)/TOTAL_AREA
 
 !RESET ACCUMULATION VARIABLES TO ZERO
 
@@ -3703,6 +3727,13 @@ TOTAL_THIC  = 0.0
 TOTAL_ZPND  = 0.0
 TOTAL_RCAN  = 0.0
 TOTAL_SCAN  = 0.0
+TOTAL_SNO   = 0.0
+TOTAL_ROF=0.0
+TOTAL_ROFO=0.0
+TOTAL_ROFS=0.0
+TOTAL_ROFB=0.0
+TOTAL_EVAP=0.0
+TOTAL_PRE=0.0
     
 ENDIF  ! IF(NCOUNT==48) THEN
 
