904d09b07b373b56aa0a7a475b26580086ea3186
soulis <soulis@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2013-07-22 09:31:44 +0000

    Just a test of the commit system
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/Branches/MESH_CODE_TEAM/Soulis/SA_MESH_1.3.006@544 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/Julian_Day_ID.f
+++ Driver/MESH_Driver/Julian_Day_ID.f
@@ -1,6 +1,7 @@
         subroutine Julian_Day_ID (year,day,jday_id) 
         INTEGER :: Year,day, jday_id
         INTEGER :: TEMP1
+! this is a test comment
         TEMP1= YEAR-1601
         jday_id = TEMP1*365+TEMP1/4+DAY 
         RETURN
