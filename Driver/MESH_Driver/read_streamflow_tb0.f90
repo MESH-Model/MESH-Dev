65a86d6bd8cbbe28c46aab3b16f0489b8bf55bfb
Daniel.Princz <Daniel.Princz@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2017-08-21 15:58:26 +0000

    Added generic procedures to read tb0 files. Updated read_streamflow_tb0.f90 to replace watroute.exe-borrowed code to use tb0 procedures from ensim_io.f90.
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/Branches/features/irrigation@1122 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/read_streamflow_tb0.f90
+++ Driver/MESH_Driver/read_streamflow_tb0.f90
@@ -1,13 +1,13 @@
 !>
 !> Description:
 !>  Subroutine to read streamflow gauge information from
-!>  MESH_input_streamflow.tb0. Adapted from read_flow_ef.f.
+!>  MESH_input_streamflow.tb0.
 !>
 !> Input:
 !*  shd: Basin shed object, containing information about the grid
 !*      definition read from MESH_drainage_database.r2c.
 !*  iun: Unit of the input file (default: 100).
-!*  fname: Full path to the file (default: './MESH_input_streamflow.tb0').
+!*  fname: Full path to the file (default: 'MESH_input_streamflow.tb0').
 !>
 subroutine read_streamflow_tb0(shd, iun, fname)
 
@@ -15,7 +15,7 @@ subroutine read_streamflow_tb0(shd, iun, fname)
     use mpi_module
     use model_dates
     use sa_mesh_shared_variables
-    use EF_Module
+    use ensim_io
 
     implicit none
 
@@ -24,110 +24,47 @@ subroutine read_streamflow_tb0(shd, iun, fname)
     integer :: iun
     character(len = *) :: fname
 
-    !> EnSim variables.
-    type(FlowParam) :: header
-    type(FlowColumnMetaData) :: colHeader
-    character(len = 10000) in_line, subString
-    character(len = 128) keyword
-    integer KeyLen, wordCount
-    logical foundEndHeader, insideColMetaData
+    !> Local variables.
+    type(ensim_keyword), dimension(:), allocatable :: vkeyword
+    integer nkeyword, ierr
+    logical verbose
 
     !> Local variables.
-    integer NS, l, ierr
+    verbose = (ro%VERBOSEMODE > 0)
 
-    !> Open the file.
-    if (ro%VERBOSEMODE > 0) print 1000, trim(adjustl(fname))
-    open(iun, file = fname, status = 'old', action = 'read', err = 997)
+    !> Open the file and read the header.
+    call open_ensim_file(iun, fname, ierr, verbose)
+    call parse_header_ensim(iun, fname, vkeyword, nkeyword, ierr)
 
-    !> Read and parse the header.
-    call InitFlowParam(header)
-    foundEndHeader = .false.
-    insideColMetaData = .false.
-    in_line(1:1) = '#'
-    do while ((.not. foundEndHeader) .and. ((in_line(1:1) == '#') .or. (in_line(1:1) == ':') .or. (len_trim(in_line) == 0)))
-
-        !> Read the line.
-        read(iun, "((a))", iostat = ierr) in_line
-        if (ierr == -1) then
-            if (ipid == 0) print "(/1x, 'ERROR: Premature end of file (EOF) encountered in: ', (a))", trim(adjustl(fname))
-            stop
-        end if
-
-        !> Replace tabs with spaces and get rid of leading whitespace.
-        call compact(in_line)
-        in_line = adjustl(in_line)
-
-        !> Parse attributes (lines that start with ':').
-        if (in_line(1:1) == ':') then
-            wordCount = SplitLine(in_line, keyword, subString)
-            keyword = lowercase(keyword)
-            KeyLen = len_trim(keyword)
-
-            !> ':endheader' is the end of the header, otherwise this is an attribute.
-            if (keyword(1:KeyLen) == ':endheader') then
-                foundEndHeader = .true.
-            else if (keyword(1:KeyLen) == ':columnmetadata') then
-                insideColMetaData = .true.
-            else if (keyword(1:KeyLen) == ':endcolumnmetadata') then
-                insideColMetaData = .false.
-            else if (insideColMetaData) then
-                ierr = ParseFlowColumnMetaData(colHeader, keyword, keyLen, subString)
-                if (ierr < 0) then
-                    if (ipid == 0) print "(1x, 'ERROR: Error parsing an attribute of the header in ', (a), ':'/, (a))", &
-                        trim(adjustl(fname)), trim(adjustl(in_line))
-                    stop
-                end if
-            else
-                ierr = ParseFlowParam(header, keyword, keyLen, subString)
-                if (ierr < 0) then
-                    if (ipid == 0) print "(1x, 'ERROR: Error parsing an attribute of the header in ', (a), ':'/, (a))", &
-                        trim(adjustl(fname)), trim(adjustl(in_line))
-                    stop
-                else if (keyword(1:KeyLen) == ':starttime') then
-
-                    !> Start date of measured/observed values.
-                    call value(subString(1:4), fms%stmg%qomeas%iyear, ierr)
-                    call value(subString(6:7), fms%stmg%qomeas%imonth, ierr)
-                    call value(subString(9:10), fms%stmg%qomeas%iday, ierr)
-                    fms%stmg%qomeas%ijday = get_jday(fms%stmg%qomeas%imonth, fms%stmg%qomeas%iday, fms%stmg%qomeas%iyear)
-                    call value(subString(12:13), fms%stmg%qomeas%ihour, ierr)
-                else if (ierr == 0) then
-                    if (ro%DIAGNOSEMODE > 0 .and. ro%VERBOSEMODE > 0) then
-                        print "(1x, 'WARNING: Unrecognized attribute of header in ', (a), ': ', (a))", &
-                        trim(adjustl(fname)), trim(adjustl(keyword))
-                    end if
-                end if
-            end if
-        end if
-    end do
+    !> Get the number of gauge locations (i.e., columns) from the file.
+    call count_columns_tb0(iun, fname, vkeyword, nkeyword, fms%stmg%n, ierr)
 
     !> Return if no gauge locations are defined.
-    fms%stmg%n = colHeader%tb0cmd%colCount
-    NS = fms%stmg%n
-    if (NS == 0) return
+    if (fms%stmg%n == 0) return
 
-    !> Streamflow attributes pulled from 'fms':
-    !*  stmg: Streamflow gauge structure
-    !*  -   n: Number of elements dimensioned.
-    !*  -   name(n): ID printed to output files.
-    !*  -   y(n): Y-coordinate of outlet location.
-    !*  -   x(n): X-coordinate of outlet location.
-    !*  -   iy(n): Vertical index of the grid-cell containing the location.
-    !*  -   jx(n): Horizontal index of the grid-cell containing the location.
-    !*  -   n(n): Rank or index of the grid-cell containing the location.
-    !*  -   DA(n): Drainage area.
-
-    !> Allocate and assign attributes for the driver.
-    call allocate_streamflow_gauge_location(fms%stmg, NS, ierr)
+    !> Allocate attributes for the driver.
+    call allocate_streamflow_gauge_location(fms%stmg, fms%stmg%n, ierr)
     if (ierr /= 0) goto 998
-    fms%stmg%qomeas%dts = header%tb0p%deltaT
-    fms%stmg%meta%name = colHeader%tb0cmd%colName
-    fms%stmg%meta%y = colHeader%tb0cmd%colLocY
-    fms%stmg%meta%x = colHeader%tb0cmd%colLocX
+
+    !> Get the time-step of the records.
+    call get_keyword_value(iun, fname, vkeyword, nkeyword, ':DeltaT', fms%stmg%qomeas%dts, ierr, verbose)
+
+    !> Populate other attributes.
+    call get_keyword_value(iun, fname, vkeyword, nkeyword, ':ColumnName', fms%stmg%meta%name, fms%stmg%n, ierr, verbose)
+    call get_keyword_value(iun, fname, vkeyword, nkeyword, ':ColumnLocationY', fms%stmg%meta%y, fms%stmg%n, ierr, verbose)
+    call get_keyword_value(iun, fname, vkeyword, nkeyword, ':ColumnLocationX', fms%stmg%meta%x, fms%stmg%n, ierr, verbose)
+
+    !> Grab the start time.
+    call parse_starttime( &
+        iun, fname, vkeyword, nkeyword, &
+        fms%stmg%qomeas%iyear, fms%stmg%qomeas%imonth, fms%stmg%qomeas%iday, fms%stmg%qomeas%ihour, fms%stmg%qomeas%imins, &
+        ierr, verbose)
+    if (fms%stmg%qomeas%iyear > 0 .and. fms%stmg%qomeas%imonth > 0 .and. fms%stmg%qomeas%iday > 0) then
+        fms%stmg%qomeas%ijday = get_jday(fms%stmg%qomeas%imonth, fms%stmg%qomeas%iday, fms%stmg%qomeas%iyear)
+    end if
 
     !> Position the file to the first record.
-    rewind iun
-    call GoToStartOfData(iun)
+    call advance_past_header(iun, fname, verbose, ierr)
 
     return
 
