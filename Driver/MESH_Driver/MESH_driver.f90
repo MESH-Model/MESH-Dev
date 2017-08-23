18c07c7afd228307c325de228c62ab088df6d397
Daniel.Princz <Daniel.Princz@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2017-08-23 00:34:59 +0000

    Bug-fix: Changed WATROF (LATFLOW) to explicitly set asat_t0 to zero if it will not be calculated otherwise. GNU compiler or certain parameterizations are causing an instability between calls to WATROF (LATFLOW) from CLASSW, where the variable isn't being assigned/initialized to zero by the subroutine; thus it can be any number, of which certain values will signal invalid math in WATDRN. It's possible the correction will impact interflow (and streamflow). Updated version in MESH_driver.f90.
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/TRUNK@1130 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/MESH_driver.f90
+++ Driver/MESH_Driver/MESH_driver.f90
@@ -146,7 +146,7 @@ program RUNMESH
 
     !* VERSION: MESH_DRIVER VERSION
     !* RELEASE: PROGRAM RELEASE VERSIONS
-    character(24) :: VERSION = '1107'
+    character(24) :: VERSION = '1130'
     character(8) RELEASE
 
     integer i, j, k, l, m, u
