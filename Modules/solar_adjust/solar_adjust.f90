736cc101b0300a10a6a6e4e525b1d937b4ac60aa
Daniel.Princz <Daniel.Princz@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2018-02-02 19:22:34 +0000

    Moved constants into subroutine. Isolated parameters. Renamed some variables. Grouped and marked temporary code that isn't critical to the subroutine (i.e., to be replaced by driver), including I/O of radiations. Replaced iterations of cells with array notation.
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/Branches/features/Solar_Adjust@1266 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Modules/solar_adjust/solar_adjust.f90
+++ Modules/solar_adjust/solar_adjust.f90
@@ -8,21 +8,12 @@ program Solar_Adjust
 
     implicit none
 
-    !> Constants.
-    real, parameter :: pi = 3.141592653589793
-    real, parameter :: DEGtoRAD = pi/180.0
-    real, parameter :: DEGtoRAD365 = 2.0*pi/365.0
-    real, parameter :: CalcFreq = 288.0
-    real, parameter :: RADxxMIN = 2.0*pi/CalcFreq
-    real, parameter :: MINS_int = 24.0*60.0/CalcFreq
-    real, parameter :: Trans = 0.818 ! mean transmissivity of the atmosphere
-
     !> File units.
     integer, parameter :: FSIN_IUN = 200, FSDIRECT_IUN = 500, FSDIFFUSE_IUN = 501
 
     !> Input variables.
     integer NROWS, NCOLS, NSTEPS
-    real, dimension(:, :), allocatable :: ELEV, LAT, SLOPE, ASPECT
+    real, dimension(:, :), allocatable :: ELEV, ylat, SLOPE, ASPECT
 
     !> Local variables.
     integer m, n
@@ -31,12 +22,12 @@ program Solar_Adjust
     open(100, file = 'index.txt', status = 'old', action = 'read')
     read(100, *) nrows, ncols, nsteps
     close(100)
-    allocate(elev(nrows, ncols), lat(nrows, ncols), slope(nrows, ncols), aspect(nrows, ncols))
+    allocate(elev(nrows, ncols), ylat(nrows, ncols), slope(nrows, ncols), aspect(nrows, ncols))
     open(100, file = 'Grid_elev.txt', status = 'old', action = 'read')
     read(100, *) ((elev(m, n), n = 1, ncols), m = 1, nrows)
     close(100)
     open(100, file = 'Grid_lat.txt', status = 'old', action = 'read')
-    read(100, *) ((lat(m, n), n = 1, ncols), m = 1, nrows)
+    read(100, *) ((ylat(m, n), n = 1, ncols), m = 1, nrows)
     close(100)
 
     !> Optional input variables.
@@ -50,52 +41,86 @@ program Solar_Adjust
     open(FSDIRECT_IUN, file = 'rsrd_direct_new.txt', action = 'write')
     open(FSDIFFUSE_IUN, file = 'rsrd_diffuse_new.txt', action = 'write')
 
-    call calc_correction(elev, lat, slope, aspect, nrows, ncols, nsteps)
+    call calc_correction(elev, ylat, slope, aspect, nrows, ncols, nsteps)
 
     contains
 
-    subroutine calc_correction(dem, Lat, slope, aspect, nrows, ncols, nsteps)
+    subroutine calc_correction(elev, ylat, slope, aspect, nrows, ncols, nsteps)
+
+        !> Constants and conversion factors.
+        real, parameter :: pi = 3.141592653589793
+        real, parameter :: DEGtoRAD = pi/180.0
+        real, parameter :: DEGtoRAD365 = 2.0*pi/365.0
+
+        !> Parameters.
+        !* Trans: Mean transmissivity of the atmosphere.
+        !* Time_Offset: Solar time offset from local time.
+        real :: Trans = 0.818
+        real :: Time_Offset = 0.67
+        integer :: CalcFreq = 288
 
         !> Input variables.
-        real, dimension(:, :), intent(in) :: dem ! elevation grid (m)
-        real, dimension(:, :), intent(in) :: Lat ! latitude of the elevation grid
+        real, dimension(:, :), intent(in) :: elev ! elevation grid (m)
+        real, dimension(:, :), intent(in) :: ylat ! latitude of the elevation grid
         real, dimension(:, :), intent(in) :: slope
         real, dimension(:, :), intent(in) :: aspect
-        integer, intent(in) :: nrows, ncols, nsteps
 
-        !> Local variables.
-        integer syyyy, sdd, shh ! Start times
-        integer k, kk, m, n, yyyy, dd, hh
-        integer Day
-        real Time_Offset
-        real, dimension(nrows, ncols, nsteps) :: rsrd ! shortwave radiation input
-        real, dimension(nrows, ncols, nsteps) :: rsrd_direct, rsrd_diffuse
+!temp
+        integer, intent(in) :: nrows, ncols, nsteps
+        integer k, m, n
+        integer :: syyyy = 2004, sdd = 245, shh = 0 ! Start times
+        integer now_year, now_day, now_hour
+
+        !> Input/output variables.
+!temp: dimensions
+        !* rsrd_dtmin: Time-step of radiation data. [minutes].
+        !* rsrd: Incoming shortwave radiation (input).
+        !* rsrd_adjusted: Adjusted incoming shortwave radiation.
+        !* rsrd_direct: Direct component of adjusted radiation.
+        !* rsrd_diffuse: Diffuse component of adjusted radiation.
+        integer :: rsrd_dtmin
+        real, dimension(nrows, ncols, nsteps) :: rsrd
+        real, dimension(nrows, ncols, nsteps) :: &
+            rsrd_direct, rsrd_diffuse, rsrd_adjusted
+
+        !> Working variables.
+!replaced with now_day        integer Day
+        integer MINS_int
+        real RADxxMIN, Hr_Ang, Dec, Rad_vec, Sol, Cdec, Sdec
         real, dimension(nrows, ncols) :: &
             Clat, Slat, x, y, z, t1, t2, t10, t20, &
             Czen, ACzen, oam, diff, &
-            Sum_Id, Sum_Diff, Sum_Flatd, Sum_Flatf, &
-            It, cosxs0, cosxsL, Id, diffuse
+            Iterr, cosxs0, cosxsL, Idir, Idiff, Sum_Idir, Sum_Diff, Sum_Flatd, Sum_Flatf
         real, dimension(nrows, ncols, nsteps) :: Qsi, Qdirect, Qdiffuse, Qflat
-        real Hr_Ang, Dec, Rad_vec, Sol, Cdec, Sdec
 
-        Time_Offset = 0.67 ! solar time offset from local time
-        Clat = cos(Lat*DEGtoRAD)
-        Slat = sin(Lat*DEGtoRAD)
+        !> Local variables.
+        integer kk
+
+!temp
+        now_year = syyyy; now_day = sdd; now_hour = shh
+        rsrd_dtmin = 60
+
+        !> Constant over time.
+        Clat = cos(ylat*DEGtoRAD)
+        Slat = sin(ylat*DEGtoRAD)
         x = -cos(aspect*DEGtoRAD)*sin(slope*DEGtoRAD)
         y = sin(aspect*DEGtoRAD)*sin(slope*DEGtoRAD) ! compute constant
         z = cos(slope*DEGtoRAD) ! components of cos(x^s)
 
-        syyyy=2004; sdd=245; shh=0
-        yyyy=syyyy; dd=sdd; hh=shh
+        !> Time-stepping (for integral).
+        RADxxMIN = 2.0*pi/CalcFreq
+        MINS_int = 24.0*60.0/CalcFreq
+
+        !> Time-stepping (for inclination in day).
         Hr_Ang = -pi*(1.0 + Time_Offset/12.0)
 
+!temp: loop
         do k = 1, nsteps ! hourly loop over the study period
 
-            read(FSIN_IUN, *) ((rsrd(m, n, k), n = 1, ncols), m = 1, nrows)
-
-            Day = dd ! day of the calendar year
-            Dec = sin((Day - 81)*DEGtoRAD365)*0.40928 ! declination
-            Rad_vec = 0.01676*cos(pi - 0.017262*(Day - 3)) + 1 ! radius vector of the earth'S orbit
+            !> Calculate geometry.
+!replaced with now_day            Day = dd ! day of the calendar year
+            Dec = sin((now_day - 81)*DEGtoRAD365)*0.40928 ! declination
+            Rad_vec = 0.01676*cos(pi - 0.017262*(now_day - 3)) + 1 ! radius vector of the earth'S orbit
             Sol = 0.0819/(Rad_vec*Rad_vec) ! solar constant  mj/m**2*min or 117.936 mj/m**2*day
             Cdec = cos(Dec)
             Sdec = sin(Dec)
@@ -103,107 +128,113 @@ program Solar_Adjust
             t2 = (-x*Clat + z*Slat)*Sdec
             t10 = Clat*Cdec
             t20 = Slat*Sdec
-            Sum_Id = 0.0
-            Sum_Diff = 0.0
-            Sum_Flatd = 0.0
-            Sum_Flatf = 0.0
 
-            do kk = 1, 12
+            !> Accumulate radiative fluxes.
+            Sum_Idir = 0.0; Sum_Diff = 0.0; Sum_Flatd = 0.0; Sum_Flatf = 0.0
+            do kk = 1, (rsrd_dtmin/MINS_int)
                 Czen = Cdec*Clat*cos(Hr_Ang) + Sdec*Slat ! cos of zenith angle
                 ACzen = acos(Czen)
                 oam = abs(1.0/(Czen + (0.50572*(96.07995 - ACzen)**(-1.6364)))) ! oam by secant approximation from Kasten and Young (1989)
 
-                do m = 1, nrows
-                    do n = 1, ncols
-                        if (oam(m, n) < 2.9) then
-                            oam(m, n) = oam(m, n)
-                        else if (oam(m, n) < 16.38) then ! zenith < 86.5 deg
-                            diff(m, n) = 10**(2.247*log10(oam(m, n)) - 2.104)
-                            oam(m, n) = oam(m, n) - diff(m, n)
-                        else if (oam(m, n) <= 114.6) then ! zenith < 89.5 deg
-                            diff(m, n) = 10**(1.576*log10(oam(m, n)) - 1.279)
-                            oam(m, n) = oam(m, n) - diff(m, n)
-                        else
-                            oam(m, n) = 30 ! computed
-                        end if
-                    end do
-                end do
-                oam = oam*(((CalcFreq - 0.0065*dem)/CalcFreq)**5.256) ! correction according to (List 1984)
+!                do m = 1, nrows
+!                    do n = 1, ncols
+                        where (oam < 2.9)
+                            oam = oam
+                        elsewhere (oam < 16.38) ! zenith < 86.5 deg
+                            diff = 10**(2.247*log10(oam) - 2.104)
+                            oam = oam - diff
+                        elsewhere (oam <= 114.6) ! zenith < 89.5 deg
+                            diff = 10**(1.576*log10(oam) - 1.279)
+                            oam = oam - diff
+                        elsewhere
+                            oam = 30 ! computed
+                        end where
+!                    end do
+!                end do
+                oam = oam*(((CalcFreq - 0.0065*elev)/CalcFreq)**5.256) ! correction according to (List 1984)
 
-                do m = 1, nrows
-                    do n = 1, ncols
-                        if (Czen(m, n) > 0) then
-                            It(m, n) = MINS_int*Sol*Czen(m, n) ! extra-ter. rad for MINS_int minute interval
+!                do m = 1, nrows
+!                    do n = 1, ncols
+                        where (Czen > 0)
+                            Iterr = MINS_int*Sol*Czen ! extra-ter. rad for MINS_int minute interval
                             cosxs0 = t10*cos(Hr_Ang)
                             cosxs0 = cosxs0 + t20
 
                             !> horizontal
-                            if (cosxs0(m, n) > 0.0) then ! not in shadow
-                                Id(m, n) = MINS_int*Sol*cosxs0(m, n)*(Trans**oam(m, n)) ! direct rad. for MINS_int minute interval
-                                diffuse(m, n) = 0.5*(0.91*It(m, n) - Id(m, n)) ! Diffuse radiation on horizontal
-                                Sum_Flatd(m, n) = Sum_Flatd(m, n) + Id(m, n)
-                                Sum_Flatf(m, n) = Sum_Flatf(m, n) + diffuse(m, n)
+                            where (cosxs0 > 0.0) ! not in shadow
+                                Idir = MINS_int*Sol*cosxs0*(Trans**oam) ! direct rad. for MINS_int minute interval
+                                Idiff = 0.5*(0.91*Iterr - Idir) ! diffuse radiation on horizontal
+                                Sum_Flatd = Sum_Flatd + Idir
+                                Sum_Flatf = Sum_Flatf + Idiff
                                 cosxsL = -y*sin(Hr_Ang)*Cdec + t1*cos(Hr_Ang)
                                 cosxsL = cosxsL + t2
-                                if (cosxsL(m, n) > 0.0) then ! slope not in shadow
-                                    Id(m, n) = MINS_int*Sol*cosxsL(m, n)*(Trans**oam(m, n)) ! direct rad. for MINS_int minute interval
-                                    Sum_Id(m, n) = Sum_Id(m, n) + Id(m, n)
-                                end if
-                                diffuse(m, n) = diffuse(m, n)*(cos(slope(m, n)/2.0))**2.0 ! on slope
-                                Sum_Diff(m, n) = Sum_Diff(m, n) + diffuse(m, n)
-                            end if
-                        end if
-                    end do
-                end do
+                                where (cosxsL > 0.0) ! slope not in shadow
+                                    Idir = MINS_int*Sol*cosxsL*(Trans**oam) ! direct rad. for MINS_int minute interval
+                                    Sum_Idir = Sum_Idir + Idir
+                                end where
+                                Idiff = Idiff*(cos(slope/2.0))**2.0 ! on slope
+                                Sum_Diff = Sum_Diff + Idiff
+                            end where
+                        end where
+!                    end do
+!                end do
                 Hr_Ang = Hr_Ang + RADxxMIN
             end do
 
-            Qdirect(:, :, k) = (1000000.0/3600.0)*Sum_Id ! clear-sky direct radiation on slope (MJ/m^2.int to W/m^2)
+            !> Convert units.
+            Qdirect(:, :, k) = (1000000.0/3600.0)*Sum_Idir ! clear-sky direct radiation on slope (MJ/m^2.int to W/m^2)
             Qdiffuse(:, :, k) = (1000000.0/3600.0)*Sum_Diff ! clear-sky diffuse radiation on slope (MJ/m^2.int to W/m^2)
             Qflat(:, :, k)= (1000000.0/3600.0)*(Sum_Flatd + Sum_Flatf) ! clear-sky 'Qdirect + Qdiffuse' on horizontal surface (MJ/m^2.int to W/m^2)
 
-            hh = hh + 1
-            if (hh > 23) then
-                print *, yyyy, dd
-                dd = dd + 1; hh = 0
-                Hr_Ang = -pi*(1.0 + Time_Offset/12.0)
-                if (dd >= 366) then
-                    if (mod(yyyy, 400) == 0) then !LEAP YEAR
-                        if (dd == 367) then
-                            dd = 1
-                            yyyy = yyyy + 1
+            !> Increment time-step information.
+            now_hour = now_hour + 1
+            if (now_hour > 23) then
+                print *, now_year, now_day
+                now_day = now_day + 1; now_hour = 0
+                if (now_day >= 366) then
+                    if (mod(now_year, 400) == 0) then !LEAP YEAR
+                        if (now_day == 367) then
+                            now_day = 1
+                            now_year = now_year + 1
                         end if
-                    else if (mod(yyyy, 100) == 0) then !NOT A LEAP YEAR
-                        dd = 1
-                        yyyy = yyyy + 1
-                    else if (mod(yyyy, 4) == 0) then !LEAP YEAR
-                        if (dd == 367) then
-                            dd = 1
-                            yyyy = yyyy + 1
+                    else if (mod(now_year, 100) == 0) then !NOT A LEAP YEAR
+                        now_day = 1
+                        now_year = now_year + 1
+                    else if (mod(now_year, 4) == 0) then !LEAP YEAR
+                        if (now_day == 367) then
+                            now_day = 1
+                            now_year = now_year + 1
                         end if
                     else !NOT A LEAP YEAR
-                        dd = 1
-                        yyyy = yyyy + 1
+                        now_day = 1
+                        now_year = now_year + 1
                     end if
                 end if
+
+                !> Time-stepping (for inclination in day).
+                Hr_Ang = -pi*(1.0 + Time_Offset/12.0)
             end if
 !        end do
+
+!temp
+            read(FSIN_IUN, *) ((rsrd(m, n, k), n = 1, ncols), m = 1, nrows)
 
         !> CRHM radiation correction for slope (the slope module of CHRM)
         Qsi = rsrd ! GEM sortwave radiation input for MESH
 !        do k = 1, nsteps
-            do m = 1, nrows
-                do n = 1, ncols
-                    if (Qflat(m, n, k) > 1.0) then
-                        rsrd_direct(m, n, k) = (Qsi(m, n, k)/Qflat(m, n, k))*Qdirect(m, n, k)
-                        rsrd_diffuse(m, n, k) = (Qsi(m, n, k)/Qflat(m, n, k))*Qdiffuse(m, n, k)
-                    else
-                        rsrd_direct(m, n, k) = 0.0
-                        rsrd_diffuse(m, n, k) = 0.0
-                    end if
-                end do
-            end do
+!            do m = 1, nrows
+!                do n = 1, ncols
+                    where (Qflat(:, :, k) > 1.0)
+                        rsrd_direct(:, :, k) = (Qsi(:, :, k)/Qflat(:, :, k))*Qdirect(:, :, k)
+                        rsrd_diffuse(:, :, k) = (Qsi(:, :, k)/Qflat(:, :, k))*Qdiffuse(:, :, k)
+                    elsewhere
+                        rsrd_direct(:, :, k) = 0.0
+                        rsrd_diffuse(:, :, k) = 0.0
+                    end where
+!                end do
+!            end do
 
+!temp
             write(FSDIRECT_IUN, '(9999(f10.3, 1x))') ((rsrd_direct(m, n, k), n = 1, ncols), m = 1, nrows)
             write(FSDIFFUSE_IUN, '(9999(f10.3, 1x))') ((rsrd_diffuse(m, n, k), n = 1, ncols), m = 1, nrows)
 
