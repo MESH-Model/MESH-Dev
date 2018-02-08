4f0ef55014571f09e24f341461d41ca8e6e9a3c6
Daniel.Princz <Daniel.Princz@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2018-02-08 19:53:01 +0000

    Added missing 'use' for 'mpi_module', required for compiling serial distribution.
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/TRUNK@1285 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/stop_program.f90
+++ Driver/MESH_Driver/stop_program.f90
@@ -2,11 +2,14 @@
 !>  Stop the program.
 subroutine stop_program()
 
+    !> 'mpi_module': Required for call to 'MPI_Finalize'.
+    use mpi_module
+
     !> Local variables.
     integer ierr
 
     !> Finalize MPI processes.
-    call mpi_finalize(ierr)
+    call MPI_Finalize(ierr)
     if (ierr /= 0) print 1000, 'MPI exchange failed to exit with normal status.'
 
     !> Stop the program.
