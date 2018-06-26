d9f1ec184063836d21cc0b5c8abd0fa04b6772b8
Juliane.Mai <Juliane.Mai@04c54e49-724f-4ac9-b36f-5436cbaa49b4>
2018-06-26 20:37:31 +0000

    Driver/MESH_Driver/sa_mesh_common.f90: adding 'only' to some 'use' statements for better readability
    git-svn-id: svn://watflood.uwaterloo.ca/mesh/Branches/people/Mai/trunk@1439 04c54e49-724f-4ac9-b36f-5436cbaa49b4

--- Driver/MESH_Driver/sa_mesh_common.f90
+++ Driver/MESH_Driver/sa_mesh_common.f90
@@ -3,17 +3,36 @@
 !>
 module sa_mesh_common
 
-    !> Parameters and variables.
-    use control_variables
-    use shd_variables
-    use fm_variables
-    use input_parameters
-    use model_variables
-    use output_variables
+  !> Parameters and variables.
+  use control_variables, only:                 &
+       run_options, ro
+  use shd_variables,     only:                 &
+       CoordSysParams, ContribElemsParams,     &
+       SoilLayer, LandElemsParams, GridParams, &
+       LandGridParams, ShedGridParams,         &       
+       SHDFILEFLAG, SHDFILEFMT, SHDTOMAPFLAG
+  use fm_variables,      only:                 &
+       fm_config_file,                         &
+       outlet_location,                        &
+       time_series,                            &
+       release_outlet,                         &
+       streamflow_gauge_location,              &
+       reservoir_outlet_location,              &
+       abstraction_point_location,             &
+       forms, fms,                             &
+       allocate_outlet_location,               &
+       allocate_time_series,                   &
+       allocate_release_outlet,                &
+       allocate_streamflow_gauge_location,     &
+       allocate_reservoir_outlet_location,     &
+       allocate_abstraction_point_location
+  use input_parameters
+  use model_variables
+  use output_variables
 
-    !> Utilities.
-    use print_routines
+  !> Utilities.
+  use print_routines  
 
-    implicit none
+  implicit none
 
-end module
+end module sa_mesh_common
