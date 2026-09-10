f64550e9a688c084f0cb40b90bf6654f7a544c83
MESH (Standalone MESH) <MESH@ec.gc.ca>
2026-09-10 11:11:30 -0600

    Updated version in MESH_driver.f90

--- Driver/MESH_Driver/MESH_driver.f90
+++ Driver/MESH_Driver/MESH_driver.f90
@@ -114,7 +114,7 @@ program RUNMESH
     !*  RELEASE: MESH family/program release.
     !*  VERSION: MESH_DRIVER version.
     character(len = DEFAULT_FIELD_LENGTH), parameter :: RELEASE = '1.5'
-    character(len = DEFAULT_FIELD_LENGTH), parameter :: VERSION = '1.5.7'
+    character(len = DEFAULT_FIELD_LENGTH), parameter :: VERSION = '1.5.8'
 
     !> Local variables.
     character(len = DEFAULT_LINE_LENGTH) RELEASE_STRING
